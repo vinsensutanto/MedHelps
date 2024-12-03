21a6229326fd5638f95162f029eefe4311bbb02d
vinsensutanto <vinsensutanto@gmail.com>
2024-12-03 11:05:36 +0700

    bootstrap installation

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,10 @@ Route::get('/contact', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
